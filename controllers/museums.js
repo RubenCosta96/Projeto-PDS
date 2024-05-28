77a7f02323c6d27b086b2be4b9df8ab19ba28ceb
diasdiogo11 <a26534@alunos.ipca.pt>
2024-05-28 17:41:23 +0100

    Services

--- controllers/museums.js
+++ controllers/museums.js
@@ -1,26 +1,11 @@
 const db = require('../config/mysql');
 const utils = require('../utils/index');
+const services = require("../Services/museums");
 
 exports.getMuseums = async (req, res) => {
 	try {
-		let museums = await db.museum.findAll();
-
-		if (museums.length === 0) return res.status(404).send({ success: 0, message: "Não existem museus" });
-
-		let response = {
-			success: 1,
-			length: museums.length,
-			results: museums.map((museum) => {
-				return {
-					id: museum.mid,
-					name: museum.museum_name,
-                    address: museum.museum_address,
-                    category: museum.museum_categorymcid,
-					zip: museum.zip_codezipid,
-					zip_ext: museum.zip_ext,
-				};
-			}),
-		};
+		
+		let response = await services.getMuseums();
 
 		return res.status(200).send(response);
 	} catch (err) {
@@ -32,24 +17,7 @@ exports.getMuseumById = async (req, res) => {
 	try {
 		let id = req.params.id;
 
-		let museum = await db.museum.findByPk(id);
-
-		if (!museum) {
-			return res.status(404).send({ success: 0, message: "Museu inexistente" });
-		}
-
-		let response = {
-			success: 1,
-			length: 1,
-			results: [{
-				id: museum.mid,
-				name: museum.museum_name,
-                address: museum.museum_address,
-                category: museum.museum_categorymcid,
-				zip: museum.zip_codezipid,
-				zip_ext: museum.zip_ext,
-			}],
-		};
+		let response = await services.getMuseumById(id);
 
 		return res.status(200).send(response);
 	} catch (err) {
@@ -61,26 +29,7 @@ exports.getMuseumsByName = async (req, res) => {
     try {
 		let name = req.params.name;
 
-		let museums = await db.museum.findAll({ where: { museum_name: name } });
-
-		if (museums.length === 0) {
-			return res.status(404).send({ success: 0, message: "Não existem museus com este nome." });
-		}
-
-		let response = {
-			success: 1,
-			length: museums.length,
-			results: museums.map((museum) => {
-				return {
-					id: museum.mid,
-					name: museum.museum_name,
-                    address: museum.museum_address,
-                    category: museum.museum_categorymcid,
-					zip: museum.zip_codezipid,
-					zip_ext: museum.zip_ext,
-				};
-			}),
-		};
+		let response = await services.getMuseumByName(name);
 
 		return res.status(200).send(response);
 	} catch (err) {
@@ -92,32 +41,7 @@ exports.getMuseumsByCategory = async (req, res) => {
     try {
         let categoryName = req.params.categoryName;
 
-        let category = await db.museum_category.findOne({ where: { mc_description : categoryName } });
-
-        if (!category) {
-            return res.status(404).send({ success: 0, message: "Categoria inexistente" });
-        }
-
-        let museums = await db.museum.findAll({ where: { museum_categorymcid: category.mcid } });
-
-        if (!museums) {
-            return res.status(404).send({ success: 0, message: "Não há museus para esta categoria" });
-        }
-
-        let response = {
-			success: 1,
-			length: museums.length,
-			results: museums.map((museum) => {
-				return {
-					id: museum.mid,
-					name: museum.museum_name,
-                    address: museum.museum_address,
-                    category: museum.museum_categorymcid,
-					zip: museum.zip_codezipid,
-					zip_ext: museum.zip_ext,
-				};
-			}),
-		};
+        let response = await services.getMuseumByCategory(categoryName);
 
         return res.status(200).send(response);
     } catch (err) {
@@ -130,38 +54,12 @@ exports.addMuseum = async (req, res) => {
 
 		let name = req.body.name;
 		let address = req.body.address;
-		let premium = req.body.premium;
 		let zip_ext = req.body.zip_ext;
 		let category = req.body.category;
 		let zip_code = req.body.zip_code;
-		//let idOwner = req.body.idOwner;
-		//let idUserToken = req.user.id;
+		let idUserToken = req.user.id;
 
-		/*let isAdmin = await utils.isAdmin(idUserToken);
-		if (!isAdmin && idOwner != idUserToken) {
-			return res.status(403).send({ success: 0, message: "Sem permissão" });
-		}
-		*/
-
-		/*let user = await db.user.findByPk(idOwner);
-		if (!user) {
-			return res.status(404).send({ success: 0, message: "Utilizador inexistente" });
-		}
-		*/
-
-		let newMuseum = await db.museum.create({
-			museum_name: name,
-			museum_address: address,
-			premium: premium,
-			zip_ext: zip_ext,
-			museum_categorymcid: category,
-			zip_codezipid: zip_code,
-		});
-
-		let response = {
-			success: 1,
-			message: "Museu criado com sucesso",
-		};
+		let response = await services.addMuseum(idUserToken, name, address, zip_ext, category, zip_code);
 
 		return res.status(200).send(response);
 	} catch (err) {
@@ -173,32 +71,9 @@ exports.editMuseum = async (req, res) => {
 	try {
 		let id = req.params.id;
 		let idUserToken = req.user.id;
-
-		let museum = await db.museum.findByPk(id);
-
-		if (!museum) {
-			return res.status(404).send({ success: 0, message: "Museu inexistente" });
-		}
-
-		let isAdmin = await utils.isAdmin(idUserToken);
-		let isManager = await utils.isManager(idUserToken);
-		if (!isAdmin || !isManager) {
-			return res.status(403).send({ success: 0, message: "Sem permissão" });
-		}
-
 		let { name, address, category, zip_code } = req.body;
 
-		museum.museum_name = name;
-		museum.museum_address = address;
-		museum.museum_categorymcid = category;
-		museum.zip_codezipid = zip_code;
-
-		await museum.save();
-
-		let response = {
-			success: 1,
-			message: "Museu editado com sucesso",
-		};
+		let response = await services.editMuseum(idUserToken, id, name, address, zip_ext, category, zip_code );
 
 		return res.status(200).send(response);
 	} catch (err) {
@@ -212,24 +87,7 @@ exports.removeMuseum = async (req, res) => {
 		let id = req.params.id;
 		let idUserToken = req.user.id;
 
-		let museum = await db.museum.findByPk(id);
-
-		if (!museum) {
-			return res.status(404).send({ success: 0, message: "Museu inexistente" });
-		}
-
-		let isAdmin = await utils.isAdmin(idUserToken);
-		let isManager = await utils.isManager(idUserToken);
-		if (!isAdmin || !isManager) {
-			return res.status(403).send({ success: 0, message: "Sem permissão" });
-		}
-
-		await museum.destroy();
-
-		let response = {
-			success: 1,
-			message: "Museu removido com sucesso",
-		};
+		let response = await services.removeMuseum(idUserToken, id);
 
 		return res.status(200).send(response);
 	} catch (err) {
@@ -243,28 +101,7 @@ exports.approveMuseum = async (req, res) => {
 		let id = req.params.id;
 		let idUserToken = req.user.id;
 
-		let museum = await db.museum.findByPk(id);
-
-		if (!museum) {
-			return res.status(404).send({ success: 0, message: "Museu inexistente" });
-		}
-
-		let isAdmin = await utils.isAdmin(idUserToken);
-		if (!isAdmin) {
-			return res.status(403).send({ success: 0, message: "Sem permissão" });
-		}
-
-		if (museum.approved === 1) {
-			return res.status(409).send({ success: 0, message: "Museu já aprovado" });
-		}
-
-		museum.approved = 1;
-		await museum.save();
-
-		let response = {
-			success: 1,
-			message: "Museu aprovado com sucesso",
-		};
+		let response = await services.approveMuseum(idUserToken, id);
 
 		return res.status(200).send(response);
 	} catch (err) {
--- controllers/product_evaluations.js
+++ controllers/product_evaluations.js
@@ -1,26 +1,12 @@
-const { where } = require("sequelize");
 const db = require("../config/mysql");
 const utils = require("../utils/index");
+const services = require("../Services/product_evaluations");
 
 exports.getAllEvaluations = async (req, res) => {
   try {
-      let evaluations = await db.product_evaluation.findAll();
 
-      if (evaluations.length === 0)
-        return res.status(404).send({ success: 0, message: "Não existe nenhuma avaliaçao" });
-
-      let response = {
-        success: 1,
-        length: evaluations.length,
-        results: evaluations.map((product_evaluation) => {
-          return {
-            description: product_evaluation.pe_description,
-            evaluation: product_evaluation.pe_evaluation,
-            userId: product_evaluation.useruid,
-            productId: product_evaluation.productprodid,
-          };
-        }),
-      };
+    let response = await services.getAllEvaluations();
+      
     return res.status(200).send(response);
   } catch (err) {
     return res.status(500).send({ error: err, message: err.message });
@@ -31,27 +17,9 @@ exports.getAllEvaluations = async (req, res) => {
 exports.getEvaluationsByUser = async (req, res) => {
     try {
         let id = req.params.id;
-        let evaluations = await db.product_evaluation.findAll({
-            where:{
-                useruid: id,
-            },
-        });
-  
-        if (evaluations.length === 0)
-            return res.status(404).send({ success: 0, message: "Não existem avaliaçoes realizadas por esse utilzador" });
-  
-        let response = {
-            success: 1,
-            length: evaluations.length,
-            results: evaluations.map((product_evaluation) => {
-            return {
-                description: product_evaluation.pe_description,
-                evaluation: product_evaluation.pe_evaluation,
-                userId: product_evaluation.useruid,
-                productId: product_evaluation.productprodid,
-            };
-            }),
-        };
+
+        let response = await services.getEvaluationsByUser(id);
+        
         return res.status(200).send(response);
         } catch (err) {
             return res.status(500).send({ error: err, message: err.message });
@@ -61,27 +29,9 @@ exports.getEvaluationsByUser = async (req, res) => {
 exports.getEvaluationsByProduct = async (req, res) => {
     try {
         let id = req.params.id;
-        let evaluations = await db.product_evaluation.findAll({
-            where:{
-                productprodid: id,
-            },
-        });
-    
-        if (evaluations.length === 0)
-            return res.status(404).send({ success: 0, message: "Não existem avaliaçoes realizadas por esse produto" });
-    
-        let response = {
-            success: 1,
-            length: evaluations.length,
-            results: evaluations.map((product_evaluation) => {
-            return {
-                description: product_evaluation.pe_description,
-                evaluation: product_evaluation.pe_evaluation,
-                userId: product_evaluation.useruid,
-                productId: product_evaluation.productprodid,
-            };
-            }),
-        };
+
+        let response = await services.getEvaluationsByProduct(id);
+       
         return res.status(200).send(response);
         } catch (err) {
             return res.status(500).send({ error: err, message: err.message });
@@ -92,23 +42,8 @@ exports.getEvaluationsByProduct = async (req, res) => {
 exports.getEvaluation = async (req, res) => {
   try {
     let id = req.params.id;
-    let result = await db.product_evaluation.findByPk(id);
-
-    if (!result) {
-      return res
-        .status(404)
-        .send({ success: 0, message: "Avaliaçao inexistente" });
-    }
-
-    let response = {    
-      success: 1,
-      results: {
-        description: result.pe_description,
-        evaluation: result.pe_evaluation,
-        userId: result.useruid,
-        productId: result.productprodid,
-      },
-    };
+    
+    let response = await services.getEvaluation(id);
 
     return res.status(200).send(response);
   } catch (err) {
@@ -121,37 +56,10 @@ exports.addEvaluation = async (req, res) => {
   try {
     let description = req.body.description;
     let evaluation = req.body.evaluation;
-    let prodId = req.body.productId;
-    let idOwner = req.body.userId;
+    let prodId = req.params.productId;
     let idUserToken = req.user.id;
 
-    let isManager = await utils.isManager(idUserToken);
-    let isAdmin = await utils.isAdmin(idUserToken);
-
-    if (!isManager && idOwner != idUserToken && isAdmin) {
-      return res.status(403).send({ success: 0, message: "Sem permissão" });
-    }
-
-    let user = await db.user.findByPk(idOwner);
-    if (!user) {
-      return res
-        .status(404)
-        .send({ success: 0, message: "Utilizador inexistente" });
-    }
-
-    //Verificaçao para entradas repetidas nas BD
-    
-    let newEvaluation = await db.product_evaluation.create({
-        pe_description: description,
-        pe_evaluation: evaluation,
-        useruid: idOwner,
-        productprodid: prodId,
-    });
-
-    let response = {
-      success: 1,
-      message: "Avaliação registada com sucesso",
-    };
+    let response = await services.addEvaluation(idUserToken, description, evaluation, prodId);
 
     return res.status(200).send(response);
   } catch (err) {
@@ -166,39 +74,9 @@ exports.editEvaluation = async (req, res) => {
     let id = req.params.id;
     let prodId = req.body.productId;
     let idUserToken = req.user.id;
+    let { description , evaluation} = req.body;
 
-    let evaluations = await db.product_evaluation.findOne({
-        where:{
-            productprodid: prodId,
-            useruid: id,
-        }
-    });
-
-    if (!evaluations) {
-      return res
-        .status(404)
-        .send({ success: 0, message: "Avaliação inexistente" });
-    }
-
-    let isManager = await utils.isManager(idUserToken); //Verificar
-    let isAdmin = await utils.isAdmin(idUserToken); //Verificar
-
-    if (!isManager && !isAdmin) {
-      return res.status(403).send({ success: 0, message: "Sem permissão" });
-    }
-
-    let { description , evaluation, userId } = req.body;
-
-    if (description) evaluations.pe_description = description;
-    if (evaluation) evaluations.pe_evaluation = evaluation;
-    if (userId) evaluations.useruid = userId;
-
-    await evaluations.save();
-
-    let response = {
-      success: 1,
-      message: "Avaliação editada com sucesso",
-    };
+    let response = await services.editEvaluation(idUserToken, description, evaluation, prodId);
 
     return res.status(200).send(response);
   } catch (err) {
@@ -213,35 +91,7 @@ exports.removeEvaluation = async (req, res) => {
     let prodId = req.body.productId;
     let idUserToken = req.user.id;
 
-    const evaluation = await await db.product_evaluation.findOne({
-        where:{
-            productprodid: prodId,
-            useruid: id,
-        }
-    });
-
-    if (!evaluation) {
-      return res
-        .status(404)
-        .send({ success: 0, message: "Avaliação inexistente" });
-    }
-
-    //let idOwner = artist.id_user; //ver se faz sentido
-
-    let isAdmin = await utils.isAdmin(idUserToken); //Verificar
-
-    if (!isAdmin) {
-      return res.status(403).send({ success: 0, message: "Sem permissão" });
-    }
-
-    //Antes verificar se esta atribuido a algum produto, se sim nao permitir
-
-    await evaluation.destroy();
-
-    let response = {
-      success: 1,
-      message: "Avaliação removida com sucesso",
-    };
+    let response = await services.removeEvaluation(idUserToken, prodId, id);
 
     return res.status(200).send(response);
   } catch (err) {
