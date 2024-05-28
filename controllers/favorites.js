d4037151eeda78b7a801785290f914f6570299ac
RubenCosta96 <a12057@alunos.ipca.pt>
2024-05-28 23:25:35 +0100

    fav/supp_states

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -1,104 +1,52 @@
 const db = require("../config/mysql");
+const db = require("../Services/favorites");
 
 exports.getFavorites = async (req, res) => {
-  try {
-    let favorites = await db.favorites.findAll();
+    try {
+        let response = await services.getFavorites();
 
-    if (favorites.length === 0) {
-      return res.status(404).send({ success: 0, message: "Não existem favoritos" });
+        return res.status(200).send(response);
+    } catch (err) {
+        return res.status(500).send({ error: err, message: err.message });
     }
-
-    let response = {
-      success: 1,
-      length: favorites.length,
-      results: favorites.map((favorite) => {
-        return {
-          user_id: favorite.useruid,
-          product_id: favorite.productprodid,
-        };
-      }),
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    return res.status(500).send({ error: err, message: err.message });
-  }
 };
-
 
 exports.getFavoriteById = async (req, res) => {
     try {
-      let id = req.params.id;
-  
-      let favorite = await db.piece_category.findByPk(id);
-  
-      if (!favorite) {
-        return res.status(404).send({ success: 0, message: "Categoria de peça inexistente" });
-      }
-  
-      let response = {
-        success: 1,
-        length: 1,
-        results: [
-          {
-            user_id: favorite.useruid,
-            product_id: favorite.productprodid,
-          },
-        ],
-      };
-  
-      return res.status(200).send(response);
+        let id = req.params.id;
+
+        let response = await services.getFavoriteById(id);
+
+        return res.status(200).send(response);
     } catch (err) {
-      return res.status(500).send({ error: err, message: err.message });
+        return res.status(500).send({ error: err, message: err.message });
     }
-  };
+};
 
 exports.addFavorite = async (req, res) => {
-  try {
-    let { user_id, product_id } = req.body;
+    try {
+        let product_id = req.body.product_id;
+        let idUserToken = req.user.id;
 
-    let newFavorite = await db.favorites.create({
-      useruid: user_id,
-      productprodid: product_id,
-    });
+        let response = await services.addFavorite(idUserToken, product_id);
 
-    let response = {
-      success: 1,
-      message: "Favorito adicionado com sucesso",
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    console.error("Error adding favorite:", err);
-    return res.status(500).send({ error: err, message: err.message });
-  }
+        return res.status(200).send(response);
+    } catch (err) {
+        console.error("Error adding favorite:", err);
+        return res.status(500).send({ error: err, message: err.message });
+    }
 };
 
 exports.removeFavorite = async (req, res) => {
-  try {
-    let { user_id, product_id } = req.body;
+    try {
+        let id = req.params.id;
+        let idUserToken = req.user.id;
 
-    const favorite = await db.favorites.findOne({
-      where: {
-        useruid: user_id,
-        productprodid: product_id
-      }
-    });
+        let response = await services.removeFavorite(idUserToken, id);
 
-    if (!favorite) {
-      return res.status(404).send({ success: 0, message: "Favorito inexistente" });
+        return res.status(200).send(response);
+    } catch (err) {
+        console.error("Error removing favorite:", err);
+        return res.status(500).send({ error: err, message: err.message });
     }
-
-    await favorite.destroy();
-
-    let response = {
-      success: 1,
-      message: "Favorito removido com sucesso",
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    console.error("Error removing favorite:", err);
-    return res.status(500).send({ error: err, message: err.message });
-  }
 };
--- controllers/support_states.js
+++ controllers/support_states.js
@@ -1,140 +1,72 @@
-const db = require('../config/mysql');
+const db = require("../config/mysql");
 const utils = require("../utils/index");
+const services = require("../services/support_states");
 
-exports.getAllSupportStates = async (req, res) =>{
-    try{
-        let idUserToken = req.user.id;
-
-        let isAdmin = await utils.isAdmin(idUserToken);
-		if (!isAdmin) return res.status(403).send({ success: 0, message: 'Sem permissão' });
-
-        let states = await db.support_state.findAll();
-
-        if (states.length === 0)
-			return res.status(404).send({ success: 0, message: "Não existem estados de suporte" });
-
-        let response = {
-			success: 1,
-			length: states.length,
-			results: states.map((support_state) => {
-				return {
-                    id: support_state.ssid,
-                    description: support_state.description,
-				};
-			}),
-		};
+exports.getAllSupportStates = async (req, res) => {
+    try {
+        let response = await services.getAllSupportStates();
 
         return res.status(200).send(response);
-    }catch (err) {
-		return res.status(500).send({ error: err, message: err.message });
-	}
+    } catch (err) {
+        return res.status(500).send({ error: err, message: err.message });
+    }
 };
 
-exports.getSupportState = async (req, res) =>{
-    try{
-        let idUserToken = req.user.id;
+exports.getSupportState = async (req, res) => {
+    try {
         let id = req.params.id;
 
-        let isAdmin = await utils.isAdmin(idUserToken);
-		if (!isAdmin) return res.status(403).send({ success: 0, message: 'Sem permissão' });
-
-        let state = await db.support_state.findByPk(id);
-
-        if (!state)
-			return res.status(404).send({ success: 0, message: "Estado inexistente" });
-
-        let response = {
-            success: 1,
-            length: 1,
-            results: [
-                {
-                    id: support_state.ssid,
-                    description: support_state.description,
-                },
-            ],
-        };
+        let response = await services.getSupportState(id);
 
         return res.status(200).send(response);
-    }catch (err) {
-		return res.status(500).send({ error: err, message: err.message });
-	}
+    } catch (err) {
+        return res.status(500).send({ error: err, message: err.message });
+    }
 };
 
-exports.addSupportState = async (req, res) =>{
-    try{
+exports.addSupportState = async (req, res) => {
+    try {
         let idUserToken = req.user.id;
         let ss_description = req.body.description;
 
-        let isAdmin = await utils.isAdmin(idUserToken);
-		if (!isAdmin) return res.status(403).send({ success: 0, message: 'Sem permissão' });
-
-        let new_Support_state = await db.support_state.create({
-            description: ss_description,
-		});
-	
-		let response = {
-			success: 1,
-			message: "Estado de suporte adicionado com sucesso",
-		};
+        let response = await services.addSupportState(
+            idUserToken,
+            ss_description
+        );
 
         return res.status(200).send(response);
-    }catch (err) {
-		return res.status(500).send({ error: err, message: err.message });
-	}
+    } catch (err) {
+        return res.status(500).send({ error: err, message: err.message });
+    }
 };
 
-exports.removeSupportState = async (req, res) =>{
-    try{
+exports.removeSupportState = async (req, res) => {
+    try {
         let idUserToken = req.user.id;
         let id = req.params.id;
 
-        let isAdmin = await utils.isAdmin(idUserToken);
-		if (!isAdmin) return res.status(403).send({ success: 0, message: 'Sem permissão' });
-
-        let state = await db.support_state.findByPk(id);
-
-        if (!state) {
-			return res.status(404).send({ success: 0, message: "Estado inexistente" });
-		}
-
-        await state.destroy();
-
-		let response = {
-			success: 1,
-			message: "Estado removido com sucesso",
-		};
+        let response = await services.removeSupportState(idUserToken, id);
 
         return res.status(200).send(response);
-    }catch (err) {
-		return res.status(500).send({ error: err, message: err.message });
-	}
+    } catch (err) {
+        return res.status(500).send({ error: err, message: err.message });
+    }
 };
 
-exports.editSupportState = async (req, res) =>{
-    try{
+exports.editSupportState = async (req, res) => {
+    try {
         let id = req.params.id;
-		let idUserToken = req.user.id;
+        let idUserToken = req.user.id;
         let description = req.body.description;
 
-        let isAdmin = await utils.isAdmin(idUserToken);
-		if (!isAdmin) return res.status(403).send({ success: 0, message: 'Sem permissão' });
-
-        let state = await db.support_state.findByPk(id);
-        if (!state) {
-			return res.status(404).send({ success: 0, message: "Estado inexistente" });
-		}
-
-        state.description = description;
-
-        await state.save();
-
-		let response = {
-			success: 1,
-			message: "Estado editado com sucesso",
-		};
+        let response = await services.editSupportState(
+            idUserToken,
+            id,
+            description
+        );
 
         return res.status(200).send(response);
-    }catch (err) {
-		return res.status(500).send({ error: err, message: err.message });
-	}
+    } catch (err) {
+        return res.status(500).send({ error: err, message: err.message });
+    }
 };
