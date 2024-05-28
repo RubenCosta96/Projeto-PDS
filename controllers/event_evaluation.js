500a3b81fe6c34b0f57a2394a943882322417c0b
25bczz <ctdiogogoncalves@gmail.com>
2024-05-28 21:53:37 +0100

    changes in services and controllers

--- controllers/event_evaluation.js
+++ controllers/event_evaluation.js
@@ -3,147 +3,65 @@ const db = require("../config/mysql");
 const event_evaluation = require("../models/event_evaluation");
 const user = require("../models/user");
 const event_status = require("../models/event_status");
+const services = require("../Services/event_evaluation");
 
 exports.getEventsEval = async (req, res) => {
-  try {
-    let event_evaluation = await db.event_evaluation.findAll();
+	try {
+		let response = await services.getEventsEval();
 
-    if (event_evaluation.length === 0) return res.status(404).send({ success: 0, message: "Não existem avaliações de evento." });
-
-    let response = {
-      success: 1,
-      length: event_evaluation.length,
-      results: event_evaluation.map((event_evaluation) => {
-        return {
-          id: event_evaluation.eventeid,
-          description: event_evaluation.ee_description,
-          evaluation: event_evaluation.ee_evaluation,
-          user_id: event_evaluation.useruid,
-        };
-      }),
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
 exports.getEventEval = async (req, res) => {
-  try {
-    let id = req.params.id;
+	try {
+		let id = req.params.id;
 
-    let event = await db.event.findByPk(id);
+		let response = await services.getEventEval(id);
 
-    if (!event) return res.status(404).send({ success: 0, message: "Evento inexistente" });
-
-    let event_evaluation = await db.event_evaluation.findAll({
-      where:{
-        eventeid:id
-      }});
-
-    let response = {
-      success: 1,
-      length: 1,
-      results: [
-        {
-          id: event_evaluation.eventeid,
-          description: event_evaluation.ee_description,
-          evaluation: event_evaluation.ee_evaluation,
-          user_id: event_evaluation.useruid,
-        },
-      ],
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
 exports.addEventsEval = async (req, res) => {
-  try {
-    let { description, evaluation, user_id } = req.body;
+	try {
+		let { description, evaluation, user_id } = req.body;
 
-    let newEventEval = await db.event_evaluation.create({
-      ee_description: description,
-      ee_evaluation: evaluation,
-      useruid: user_id,
-    });
+		let response = await services.addEventsEval(description, evaluation, user_id);
 
-    let response = {
-      success: 1,
-      message: "Avaliação de evento criada com sucesso",
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    console.error("Error adding event evaluation:", err);
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
 exports.editEventsEval = async (req, res) => {
-  try {
-    let id = req.params.id;
-    let idUserToken = req.user.id;
-    let { description, evaluation, user_id } = req.body;
+	try {
+		let id = req.params.id;
+		let idUserToken = req.user.id;
+		let { description, evaluation, user_id } = req.body;
 
-    let event_evaluation = await db.event_evaluation.findByPk(id);
+		let response = await services.editEventsEval(id, idUserToken, description, evaluation, user_id);
 
-    if (!event_evaluation) {
-      return res.status(404).send({ success: 0, message: "Avaliação de evento inexistente" });
-    }
-
-    let user = await db.user.findByPk(idUserToken);
-    if (!user) {
-      return res.status(404).send({ success: 0, message: "Utilizador inexistente" });
-    }
-
-    event_evaluation.eventeeid = id;
-    event_evaluation.ee_description = description;
-    event_evaluation.ee_evaluation = evaluation;
-    event_evaluation.useruid = user_id;
-
-    await event_evaluation.save();
-
-    let response = {
-      success: 1,
-      message: "Avaliação de evento editada com sucesso",
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    console.error("Error editing event evaluation:", err);
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
 exports.removeEventsEval = async (req, res) => {
-  try {
-    let id = req.params.id;
-    let idUserToken = req.user.id;
-    const event_evaluation = await db.event_evaluation.findByPk(id);
+	try {
+		let id = req.params.id;
+		let idUserToken = req.user.id;
 
-    if (!event_evaluation) {
-      return res.status(404).send({ success: 0, message: "Avaliação inexistente" });
-    }
+		let response = await services.removeEventsEval(id, idUserToken);
 
-    let isAdmin = await utils.isAdmin(idUserToken); //Verificar
-    if (!isAdmin) {
-      return res.status(403).send({ success: 0, message: "Sem permissão" });
-    }
-
-    await event_evaluation.destroy();
-
-    let response = {
-      success: 1,
-      message: "Avaliação de evento removida com sucesso",
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    console.error("Error removing event evaluation:", err);
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
--- controllers/museum_evaluation.js
+++ controllers/museum_evaluation.js
@@ -1,125 +1,63 @@
 const utils = require("../utils/index");
 const db = require("../config/mysql");
+const services = require("../Services/museum_evaluation");
+const user = require("../models/user");
 
 exports.getMuseumEvaluations = async (req, res) => {
-  try {
-    let museum_evaluations = await db.museum_evaluation.findAll();
+	try {
+		let response = await services.getMuseumEvaluations();
 
-    if (museum_evaluations.length === 0) return res.status(404).send({ success: 0, message: "Não existem avaliações de museu" });
-
-    let response = {
-      success: 1,
-      length: museum_evaluations.length,
-      results: museum_evaluations.map((museum_evaluation) => {
-        return {
-          description: museum_evaluation.me_description,
-          evaluation: museum_evaluation.me_evaluation,
-        };
-      }),
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
 exports.getMuseumEvaluation = async (req, res) => {
-  try {
-    let id = req.params.id;
+	try {
+		let id = req.params.id;
 
-    let museum_evaluation = await db.museum_evaluation.findByPk(id);
+		let response = await services.getMuseumEvaluation(id);
 
-    if (!museum_evaluation) return res.status(404).send({ success: 0, message: "Avaliação inexistente" });
-
-    let response = {
-      success: 1,
-      length: 1,
-      results: [
-        {
-          description: museum_evaluation.me_description,
-          evaluation: museum_evaluation.me_evaluation,
-        },
-      ],
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
 exports.addMuseumEvaluation = async (req, res) => {
-  try {
-    let { description, evaluation} = req.body;
+	try {
+		let { description, evaluation, museumId, userId } = req.body;
 
-    let newMuseum_Evaluation = await db.museum_evaluation.create({
-      me_description: description,
-      me_evaluation: evaluation,
-      museummid: museumId,
-      useruid: userId,
-    });
+		let response = await services.addMuseumEvaluation(description, evaluation, museumId, userId);
 
-    let response = {
-      success: 1,
-      message: "Avaliação criada com sucesso",
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    console.error("Error adding evaluation:", err);
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
 exports.editMuseumEvaluation = async (req, res) => {
-  try {
-    let id = req.params.id;
-    let { description, evaluation } = req.body;
+	try {
+		let id = req.params.id;
+		let { description, evaluation } = req.body;
 
-    let museum_evaluation = await db.museum_evaluation.findByPk(id);
+		let response = await services.editMuseumEvaluation(id, description, evaluation);
 
-    if (!museum_evaluation) {
-      return res.status(404).send({ success: 0, message: "Avaliação inexistente" });
-    }
-
-    if (description) museum_evaluation.me_description = description;
-    if (evaluation) museum_evaluation.me_evaluation = evaluation;
-
-    await museum_evaluation.save();
-
-    let response = {
-      success: 1,
-      message: "Avaliação editada com sucesso",
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    console.error("Error editing evaluation:", err);
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
 exports.removeMuseumEvaluation = async (req, res) => {
-  try {
-    let id = req.params.id;
+	try {
+		let id = req.params.id;
 
-    const museum_evaluation = await db.museum_evaluation.findByPk(id);
+		let response = await services.removeMuseumEvaluation(id);
 
-    if (!museum_evaluation) {
-      return res.status(404).send({ success: 0, message: "Avaliação inexistente" });
-    }
-
-    await museum_evaluation.destroy();
-
-    let response = {
-      success: 1,
-      message: "Avaliação removida com sucesso",
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    console.error("Error removing evaluation:", err);
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
