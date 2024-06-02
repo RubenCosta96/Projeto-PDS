aeaea78b50dc73813725fef086c217df09bf7ba9
25bczz <ctdiogogoncalves@gmail.com>
2024-06-03 00:22:57 +0100

    added new services

--- controllers/products.js
+++ controllers/products.js
@@ -1,273 +1,92 @@
 const db = require("../config/mysql");
 const utils = require("../utils/index");
+const services = require("../Services/products");
 
 exports.getAllProducts = async (req, res) => {
-  try {
-    let products = await db.product.findAll();
+	try {
+		let response = await services.getAllProducts();
 
-    if (products.length === 0)
-      return res
-        .status(404)
-        .send({ success: 0, message: "Não existem produtos" });
-
-    let response = {
-      success: 1,
-      length: products.length,
-      results: products.map((product) => {
-        return {
-          id: product.prodid,
-          name: product.product_name,
-          price: product.product_price,
-          quantity: product.product_quantity,
-          museum: product.museummid,
-          type: product.product_typeptid,
-        };
-      }),
-    };
-    return res.status(200).send(response);
-  } catch (err) {
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
 exports.getProductsByMuseum = async (req, res) => {
-  try {
-    let id = req.params.id;
-    let result = await db.museum.findOne({
-      where: {
-        mid: id,
-      },
-    });
+	try {
+		let id = req.params.id;
 
-    if (!result) {
-      return res.status(404).send({ success: 0, message: "Museu inexistente" });
-    }
+		let response = await services.getProductsByMuseum(id);
 
-    let products = await db.product.findAll({
-      where: {
-        museummid: id,
-      },
-    });
-
-    if (products.length === 0)
-      return res.status(404).send({
-        success: 0,
-        message: "Não existem produtos associados a este museu",
-      });
-
-    let response = {
-      success: 1,
-      length: products.length,
-      results: products.map((product) => {
-        return {
-          id: product.prodid,
-          name: product.product_name,
-          price: product.product_price,
-          quantity: product.product_quantity,
-          museum: product.museummid,
-          type: product.product_typeptid,
-        };
-      }),
-    };
-    return res.status(200).send(response);
-  } catch (err) {
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
 exports.getProductsByCategory = async (req, res) => {
-  try {
-    let id = req.params.id;
-    let result = await db.product_type.findOne({
-      where: {
-        ptid: id,
-      },
-    });
+	try {
+		let id = req.params.id;
 
-    if (!result) {
-      return res
-        .status(404)
-        .send({ success: 0, message: "Categoria inexistente" });
-    }
+		let response = await services.getProductsByCategory(id);
 
-    let products = await db.product.findAll({
-      where: {
-        product_typeptid: id,
-      },
-    });
-
-    if (products.length === 0)
-      return res.status(404).send({
-        success: 0,
-        message: "Não existem produtos associados a esta categoria",
-      });
-
-    let response = {
-      success: 1,
-      length: products.length,
-      results: products.map((product) => {
-        return {
-          id: product.prodid,
-          name: product.product_name,
-          price: product.product_price,
-          quantity: product.product_quantity,
-          museum: product.museummid,
-          type: product.product_typeptid,
-        };
-      }),
-    };
-    return res.status(200).send(response);
-  } catch (err) {
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
 exports.getProduct = async (req, res) => {
-  try {
-    let id = req.params.id;
-    let result = await db.product.findByPk(id);
+	try {
+		let id = req.params.id;
 
-    if (!result) {
-      return res
-        .status(404)
-        .send({ success: 0, message: "Produto inexistente" });
-    }
+		let response = await services.getProduct(id);
 
-    let response = {
-      success: 1,
-      results: {
-        id: result.prodid,
-        name: result.product_name,
-        price: result.product_price,
-        quantity: result.product_quantity,
-        museum: result.museummid,
-        type: result.product_typeptid,
-      },
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
 exports.addProduct = async (req, res) => {
-  try {
-    let name = req.body.name;
-    let price = req.body.price;
-    let mid = req.body.museummid;
-    let type = req.body.type;
-    let idUserToken = req.user.id;
+	try {
+		let name = req.body.name;
+		let price = req.body.price;
+		let mid = req.body.museummid;
+		let type = req.body.type;
+		let idUserToken = req.user.id;
 
-    let isManager = await utils.isManager(idUserToken); 
-    let isAdmin = await utils.isAdmin(idUserToken);
+		let response = await services.addProduct(name, price, mid, type, idUserToken);
 
-    if (!isManager && !isAdmin) {
-      return res.status(403).send({ success: 0, message: "Sem permissão" });
-    }
-
-    let user = await db.user.findByPk(idUserToken);
-    if (!user) {
-      return res
-        .status(404)
-        .send({ success: 0, message: "Utilizador inexistente" });
-    }
-
-    //Verificaçao para entradas repetidas nas BD
-
-    let newProduct = await db.product.create({
-      product_name: name,
-      product_price: price,
-      product_quantity: 0,
-      museummid: mid,
-      product_typeptid: type,
-    });
-
-    let response = {
-      success: 1,
-      message: "Produto criado com sucesso",
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    console.error("Error adding Product:", err);
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
 exports.editProduct = async (req, res) => {
-  try {
-    let id = req.params.id;
-    let idUserToken = req.user.id;
+	try {
+		let id = req.params.id;
+		let idUserToken = req.user.id;
+		let { name, price, museummid, type } = req.body;
 
-    let product = await db.product.findByPk(id);
+		let response = await services.editProduct(id, idUserToken, name, price, museummid, type);
 
-    if (!product) {
-      return res
-        .status(404)
-        .send({ success: 0, message: "Produto inexistente" });
-    }
-
-    let isManager = await utils.isManager(idUserToken); //Verificar
-    let isAdmin = await utils.isAdmin(idUserToken); //Verificar
-
-    if (!isManager && !isAdmin) {
-      return res.status(403).send({ success: 0, message: "Sem permissão" });
-    }
-
-    let { name, price, museummid, type } = req.body;
-
-    if (name) product.product_name = name;
-    if (price) product.product_price = price;
-    if (museummid) product.museummid = museummid;
-    if (type) product.product_typeptid = type;
-    //Colocar verificações
-
-    await product.save();
-
-    let response = {
-      success: 1,
-      message: "Produto editado com sucesso",
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    console.error("Error editing Product:", err);
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
 exports.removeProduct = async (req, res) => {
-  try {
-    let id = req.params.id;
-    let idUserToken = req.user.id;
+	try {
+		let id = req.params.id;
+		let idUserToken = req.user.id;
 
-    const product = await db.product.findByPk(id);
+		let response = await services.removeProduct(id, idUserToken);
 
-    if (!product) {
-      return res
-        .status(404)
-        .send({ success: 0, message: "Produto inexistente" });
-    }
-
-    //let idOwner = artist.id_user; //ver se faz sentido
-
-    let isAdmin = await utils.isAdmin(idUserToken); //Verificar
-
-    if (!isAdmin) {
-      return res.status(403).send({ success: 0, message: "Sem permissão" });
-    }
-
-    await product.destroy();
-
-    let response = {
-      success: 1,
-      message: "Produto removido com sucesso",
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    console.error("Error removing Product:", err);
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
--- controllers/purchase_lines.js
+++ controllers/purchase_lines.js
@@ -1,205 +1,80 @@
 const { where } = require("sequelize");
 const db = require("../config/mysql");
 const utils = require("../utils/index");
+const services = require("../Services/purchase_lines");
 
 exports.getAllPurchaseLines = async (req, res) => {
-  try {
-      let purchaseLines = await db.purchase_line.findAll();
+	try {
+		let response = await services.getAllPurchaseLines();
 
-      if (purchaseLines.length === 0)
-        return res.status(404).send({ success: 0, message: "Não existe nenhuma linha registada" });
-
-      let response = {
-        success: 1,
-        length: purchaseLines.length,
-        results: purchaseLines.map((purchase_line) => {
-          return {
-            id: purchaseLines.purchase_lid,
-            quantity: purchaseLines.purline_quantity,
-            saleInvoiceId: purchaseLines.purchase_invoicepurchase_invoiceid,
-            productId: purchaseLines.productprodid,
-          };
-        }),
-      };
-    return res.status(200).send(response);
-  } catch (err) {
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
-
 exports.getLinesByPurchase = async (req, res) => {
-    try {
-        let id = req.params.id;
-        let lines = await db.purchase_line.findAll({
-            where:{
-                purchase_invoicepurchase_invoiceid: id,
-            },
-        });
-  
-        if (lines.length === 0)
-            return res.status(404).send({ success: 0, message: "Não existem linhas registadas relativas e essa compra" });
-  
-        let response = {
-            success: 1,
-            length: lines.length,
-            results: lines.map((purchase_line) => {
-            return {
-                id: purchase_line.purchase_lid,
-                quantity: purchase_line.purline_quantity,
-                saleInvoiceId: purchase_line.purchase_invoicepurchase_invoiceid,
-                productId: purchase_line.productprodid,
-            };
-            }),
-        };
-        return res.status(200).send(response);
-        } catch (err) {
-        return res.status(500).send({ error: err, message: err.message });
-        }
-    };
+	try {
+		let id = req.params.id;
 
+		let response = await services.getLinesByPurchase(id);
+
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
+};
 
 exports.getPurchaseLine = async (req, res) => {
-  try {
-    let id = req.params.id;
-    let result = await db.purchase_line.findByPk(id);
+	try {
+		let id = req.params.id;
 
-    if (!result) {
-      return res
-        .status(404)
-        .send({ success: 0, message: "Linha de compra inexistente" });
-    }
+		let response = await services.getPurchaseLine(id);
 
-    let response = {    
-      success: 1,
-      results: {
-        id: result.purchase_lid,
-        quantity: result.purline_quantity,
-        purchaseInvoiceId: result.purchase_invoicepurchase_invoiceid,
-        productId: result.productprodid,
-      },
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
 exports.addPurchaseLine = async (req, res) => {
-  try {
-    let quantity = req.body.quantity;
-    let purchaseId = req.body.PurchaseInvoiceId;
-    let prodId = req.body.productId;
-    let idUserToken = req.user.id;
+	try {
+		let quantity = req.body.quantity;
+		let purchaseId = req.body.PurchaseInvoiceId;
+		let prodId = req.body.productId;
+		let idUserToken = req.user.id;
 
-    let isManager = await utils.isManager(idUserToken);
-    let isAdmin = await utils.isAdmin(idUserToken);
+		let response = await services.addPurchaseLine(quantity, purchaseId, prodId, idUserToken);
 
-    if (!isManager && !isAdmin) {
-      return res.status(403).send({ success: 0, message: "Sem permissão" });
-    }
-
-    let user = await db.user.findByPk(idUserToken);
-    if (!user) {
-      return res
-        .status(404)
-        .send({ success: 0, message: "Utilizador inexistente" });
-    }
-    
-    let newPurchaseLine = await db.purchase_line.create({
-        purline_quantity: quantity,
-        productprodid: prodId,
-        purchase_invoicepurchase_invoiceid: purchaseId,
-    });
-
-    let response = {
-      success: 1,
-      message: "linha de compra registada com sucesso",
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    console.error("Error adding purchase Line:", err);
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
-
 exports.editPurchaseLine = async (req, res) => {
-  try {
-    let id = req.params.id;
-    let idUserToken = req.user.id;
+	try {
+		let id = req.params.id;
+		let idUserToken = req.user.id;
+		let { quantity, purchaseId, prodId } = req.body;
 
-    let purchaseLine = await db.purchase_line.findByPk(id);
+		let response = await services.editPurchaseLine(id, idUserToken, quantity, purchaseId, prodId);
 
-    if (!purchaseLine) {
-      return res
-        .status(404)
-        .send({ success: 0, message: "Linha de compra inexistente" });
-    }
-
-    let isManager = await utils.isManager(idUserToken); //Verificar
-    let isAdmin = await utils.isAdmin(idUserToken); //Verificar
-
-    if (!isManager && !isAdmin) {
-      return res.status(403).send({ success: 0, message: "Sem permissão" });
-    }
-
-    let { quantity , purchaseId, prodId } = req.body;
-
-    if (quantity) purchaseLine.purline_quantity = quantity;
-    if (purchaseId) purchaseLine.purchase_invoicepurchase_invoiceid = purchaseId;
-    if (prodId) purchaseLine.productprodid = prodId;
-    
-
-    await purchaseLine.save();
-
-    let response = {
-      success: 1,
-      message: "Linha de compra editada com sucesso",
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    console.error("Error editing purchase line:", err);
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
 
 exports.removePurchaseLine = async (req, res) => {
-  try {
-    let id = req.params.id;
-    let idUserToken = req.user.id;
+	try {
+		let id = req.params.id;
+		let idUserToken = req.user.id;
 
-    const purchaseLine = await db.purchase_line.findByPk(id);
+		let response = await services.removePurchaseLine(id, idUserToken);
 
-    if (!purchaseLine) {
-      return res
-        .status(404)
-        .send({ success: 0, message: "Linha de compra inexistente" });
-    }
-
-    //let idOwner = artist.id_user; //ver se faz sentido
-
-    let isAdmin = await utils.isAdmin(idUserToken); //Verificar
-
-    if (!isAdmin) {
-      return res.status(403).send({ success: 0, message: "Sem permissão" });
-    }
-
-    //Antes verificar se esta atribuido a algum produto, se sim nao permitir
-
-    await purchaseLine.destroy();
-
-    let response = {
-      success: 1,
-      message: "Linha de compra removida com sucesso",
-    };
-
-    return res.status(200).send(response);
-  } catch (err) {
-    console.error("Error removing purchase line:", err);
-    return res.status(500).send({ error: err, message: err.message });
-  }
+		return res.status(200).send(response);
+	} catch (err) {
+		return res.status(500).send({ error: err, message: err.message });
+	}
 };
