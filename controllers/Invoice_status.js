50a78d0e0bcff858719490b58bf325968967ba08
25bczz <ctdiogogoncalves@gmail.com>
2024-06-03 03:41:31 +0100

    last service

--- controllers/Invoice_status.js
+++ controllers/Invoice_status.js
@@ -1,27 +1,12 @@
 const { where } = require("sequelize");
 const db = require("../config/mysql");
 const utils = require("../utils/index");
+const services = require("../Services/invoice_status");
 
 exports.getAllInvoiceStatus = async (req, res) => {
 	try {
-		let status = await db.Invoice_status.findAll();
+		let response = await services.getAllInvoiceStatus();
 
-		if (status.length === 0)
-			return res.status(404).send({
-				success: 0,
-				message: "Não existe nenhum estado de faturação",
-			});
-
-		let response = {
-			success: 1,
-			length: status.length,
-			results: status.map((Invoice_status) => {
-				return {
-					id: Invoice_status.invoicestatusid,
-					description: Invoice_status.description,
-				};
-			}),
-		};
 		return res.status(200).send(response);
 	} catch (err) {
 		return res.status(500).send({ error: err, message: err.message });
@@ -31,22 +16,8 @@ exports.getAllInvoiceStatus = async (req, res) => {
 exports.getInvoiceStatus = async (req, res) => {
 	try {
 		let id = req.params.id;
-		let result = await db.Invoice_status.findByPk(id);
 
-		if (!result) {
-			return res.status(404).send({
-				success: 0,
-				message: "Estado de faturação inexistente",
-			});
-		}
-
-		let response = {
-			success: 1,
-			results: {
-				id: result.invoicestatusid,
-				description: result.description,
-			},
-		};
+		let response = await services.getInvoiceStatus(id);
 
 		return res.status(200).send(response);
 	} catch (err) {
@@ -59,31 +30,10 @@ exports.addInvoiceStatus = async (req, res) => {
 		let description = req.body.description;
 		let idUserToken = req.user.id;
 
-		let isAdmin = await utils.isAdmin(idUserToken);
-
-		if (!isAdmin) {
-			return res.status(403).send({ success: 0, message: "Sem permissão" });
-		}
-
-		let user = await db.user.findByPk(idUserToken);
-		if (!user) {
-			return res.status(404).send({ success: 0, message: "Utilizador inexistente" });
-		}
-
-		//Verificaçao para entradas repetidas nas BD
-
-		let NewInvoiceStatus = await db.Invoice_status.create({
-			description: description,
-		});
-
-		let response = {
-			success: 1,
-			message: "Estado de faturação registado com sucesso",
-		};
+		let response = await services.addInvoiceStatus(description, idUserToken);
 
 		return res.status(200).send(response);
 	} catch (err) {
-		console.error("Error adding invoice Status", err);
 		return res.status(500).send({ error: err, message: err.message });
 	}
 };
@@ -92,36 +42,12 @@ exports.editInvoiceStatus = async (req, res) => {
 	try {
 		let id = req.params.id;
 		let idUserToken = req.user.id;
-
-		let status = await db.Invoice_status.findByPk(id);
-
-		if (!status) {
-			return res.status(404).send({
-				success: 0,
-				message: "Estado de faturação inexistente",
-			});
-		}
-
-		let isAdmin = await utils.isAdmin(idUserToken); //Verificar
-
-		if (!isAdmin) {
-			return res.status(403).send({ success: 0, message: "Sem permissão" });
-		}
-
 		let description = req.body.description;
 
-		if (description) status.description = description;
-
-		await status.save();
-
-		let response = {
-			success: 1,
-			message: "Estado de faturação editado com sucesso",
-		};
+		let response = await services.editInvoiceStatus(id, idUserToken, description);
 
 		return res.status(200).send(response);
 	} catch (err) {
-		console.error("Error editing Invoice State:", err);
 		return res.status(500).send({ error: err, message: err.message });
 	}
 };
@@ -131,33 +57,10 @@ exports.removeUserState = async (req, res) => {
 		let id = req.params.id;
 		let idUserToken = req.user.id;
 
-		let status = await db.Invoice_status.findByPk(id);
-
-		if (!status) {
-			return res.status(404).send({
-				success: 0,
-				message: "Estado de faturação inexistente",
-			});
-		}
-
-		let isAdmin = await utils.isAdmin(idUserToken); //Verificar
-
-		if (!isAdmin) {
-			return res.status(403).send({ success: 0, message: "Sem permissão" });
-		}
-
-		//Antes verificar se esta atribuido a algum produto, se sim nao permitir
-
-		await status.destroy();
-
-		let response = {
-			success: 1,
-			message: "Estado de faturação removido com sucesso",
-		};
+		let response = await services.removeUserState(id, idUserToken);
 
 		return res.status(200).send(response);
 	} catch (err) {
-		console.error("Error removing Invoice Status:", err);
 		return res.status(500).send({ error: err, message: err.message });
 	}
 };
--- routes/Invoice_status.js
+++ routes/Invoice_status.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-const InvoiceStatusController = require("../controllers/Invoice_status");
+const InvoiceStatusController = require("../controllers/invoice_status");
 const login = require("../middleware/login");
 
 // list All evaluations
-router.get("/InvoiceStatus", InvoiceStatusController.getAllInvoiceStatus);
+router.get("/invoiceStatus", InvoiceStatusController.getAllInvoiceStatus);
 
 // list certain evaluation
-router.get("/InvoiceStatus/:id", InvoiceStatusController.getInvoiceStatus);
+router.get("/invoiceStatus/:id", InvoiceStatusController.getInvoiceStatus);
 
 // Add evaluations
-router.post("/InvoiceStatus/add", login.required, InvoiceStatusController.addInvoiceStatus);
+router.post("/invoiceStatus/add", login.required, InvoiceStatusController.addInvoiceStatus);
 
 // Edit evaluation
-router.put("/InvoiceStatus/edit/:id", login.required, InvoiceStatusController.editInvoiceStatus);
+router.put("/invoiceStatus/edit/:id", login.required, InvoiceStatusController.editInvoiceStatus);
 
 // Remove evaluation
-router.delete("/InvoiceStatus/remove/:id", login.required, InvoiceStatusController.removeUserState);
+router.delete("/invoiceStatus/remove/:id", login.required, InvoiceStatusController.removeUserState);
 
 module.exports = router;
